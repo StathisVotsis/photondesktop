2d8d0d182825d94d2f047589a8d09649c0dbb851
StathisVotsis <stathisvotsis@gmail.com>
2017-11-02 12:14:43 +0200

    asd

---
+++ photondesktop/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Particle.SDK;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace photondesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var success = await ParticleCloud.SharedCloud.LoginAsync(textBox1.Text, textBox2.Text);
+                if (success)
+                {
+                    MessageBox.Show("Success login");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid credentials");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invalid credentials or network error or null credentials");
+            }           
+        }
+    }
+}
